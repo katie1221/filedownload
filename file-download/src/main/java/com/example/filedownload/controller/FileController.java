83182828b7950c1c280a57efe30fccc0e38ad547
katie1221 <68039943+katie1221@users.noreply.github.com>
2023-05-10 09:55:25 +0800

    Update FileController.java
    下载支持在线打开的方式

--- file-download/src/main/java/com/example/filedownload/controller/FileController.java
+++ file-download/src/main/java/com/example/filedownload/controller/FileController.java
@@ -2,11 +2,13 @@ package com.example.filedownload.controller;
 
 import com.example.filedownload.utils.FileUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
+import java.io.*;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -98,4 +100,38 @@ public class FileController {
         FileUtil.zipUrlToFile(mapList, request, response);
     }
 
+    /**
+     * 下载支持在线打开的方式
+     * @param response
+     */
+    @RequestMapping("/download/online")
+    public void download(HttpServletResponse response,@RequestParam("isOnLine") Boolean isOnLine) throws IOException {
+        String filePath="F:\\1.png";
+        File f = new File(filePath);
+        if (!f.exists()) {
+            response.sendError(404, "File not found!");
+            return;
+        }
+        BufferedInputStream br = new BufferedInputStream(new FileInputStream(f));
+        byte[] buf = new byte[1024];
+        int len = 0;
+
+        response.reset(); // 非常重要
+        if (isOnLine) {
+            // 在线打开方式
+            URL u = new URL("file:///" + filePath);
+            response.setContentType(u.openConnection().getContentType());
+            response.setHeader("Content-Disposition", "inline; filename=" + f.getName());
+            // 文件名应该编码成UTF-8
+        } else { // 纯下载方式
+            response.setContentType("application/x-msdownload");
+            response.setHeader("Content-Disposition", "attachment; filename=" + f.getName());
+        }
+        OutputStream out = response.getOutputStream();
+        while ((len = br.read(buf)) > 0) {
+            out.write(buf, 0, len);
+        }
+        br.close();
+        out.close();
+    }
 }
